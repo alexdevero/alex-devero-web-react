33395dde871a675da0bb883506f9ec4ff2ac5b5b
Alex Devero <deveroalex@gmail.com>
2018-03-25 09:21:10 +0200

    Rename js to jsx

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -5,21 +5,25 @@ import * as React from 'react'
 import { Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Work from './pages/Work'
+import About from './pages/About'
+import Lab from './pages/Lab'
+import Contact from './pages/Contact'
 
-// import Header from './components/header'
+import Header from './components/Header'
 
 // Component Implementation
 export class Main extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        {/* <Header /> */}
+        <Header />
 
         <Route exact={true} path="/" component={Home}/>
-        {/* <Route path="/work" component={Work}/> */}
-        {/* <Route path="/about" component={About}/> */}
-        {/* <Route path="/lab" component={Lab}/> */}
-        {/* <Route path="/contact" component={Contact}/> */}
+        <Route path="/work" component={Work}/>
+        <Route path="/about" component={About}/>
+        <Route path="/lab" component={Lab}/>
+        <Route path="/contact" component={Contact}/>
 
         {/* <FooterMenu /> */}
 
