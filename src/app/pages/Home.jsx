0c6bee660cb85b0b9c21792da7c1a6bde611b200
Alex Devero <deveroalex@gmail.com>
2018-03-31 11:13:52 +0200

    Restructure content of homepage

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -6,13 +6,13 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <header className="home-header">
+        <div className="home-header">
           <h1 className="home-title">APP/UI/UX/WEB <br/>DESIGN & DEVELOPMENT</h1>
 
-          <h2>The best websites & digital products in this Universe.</h2>
+          <h2 className="home-subtitle">The best websites & digital products in this Universe.</h2>
 
-          <h3>Built for the future and delivered today.</h3>
-        </header>
+          <h3 className="home-subtitle">Built for the future and delivered today.</h3>
+        </div>
       </div>
     )
   }
